749a59a6320ccbc48c8c2d97a6b34bbec6dcdb36
CarlosCRG19 <carloscrg19@gmail.com>
2021-07-06 18:27:24 -0500

    feat(user identification): sign up, login and user persistance features added

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /*Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f")
+                .clientKey("rWFPEbTs7UzkaVsIXnQ4qmmr9oWqwXfiiJehtIZu")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );*/
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("5fMlL5b2J8lSQIxItFrHzx8A84i75LBOi1ZLmuja")
+                .clientKey("45kX5He0CGz6I9a5caVCjUuIXAHr0ZstKkrq8wSl")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
